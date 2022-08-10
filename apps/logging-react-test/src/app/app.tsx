d464a08f8cf20f5298f36635106250a7c33fbfdd
Gordon Schroeder <gordon.schroeder@alchemisten.ag>
2022-08-10 17:56:16 +0200

    style: Fix indentation

--- apps/logging-react-test/src/app/app.tsx
+++ apps/logging-react-test/src/app/app.tsx
@@ -1,22 +1,54 @@
-import { LoggerFactory } from '@alchemisten/logging';
+import { ConsoleTransport, LoggerFactory, LogLevel, LogOptions } from '@alchemisten/logging';
 import { SentryBrowserTransport } from '@alchemisten/logging-transport-sentry-browser';
 
 const logger = LoggerFactory({
-	environment: 'local',
-	transports: [
-		new SentryBrowserTransport({
-			sentryConfig: {
-				dsn: 'https://2403a222dc334e5a8a123e037b8e2a9e@o564898.ingest.sentry.io/6634249',
-			},
-		}),
-	],
+  environment: 'local',
+  transports: [
+    new ConsoleTransport({}),
+    new SentryBrowserTransport({
+      sentryConfig: {
+        dsn: 'https://2403a222dc334e5a8a123e037b8e2a9e@o564898.ingest.sentry.io/6634249',
+      },
+    }),
+  ],
 });
 
 export function App() {
-	const handleClick = () => {
-		logger.error('Bre');
-	};
-	return <div onClick={handleClick}>Click me</div>;
+  const handleClick = (level: LogLevel) => {
+    const options: LogOptions = {};
+    if (level === 'fatal' || level === 'error') {
+      options.error = new Error(`A${level === 'error' ? 'n' : ''}${level === 'fatal' ? ' fatal' : ''} error`);
+    }
+    logger[level](`This is a ${level}`, options);
+  };
+  return (
+    <>
+      <button type="button" onClick={() => handleClick('trace')}>
+        Trace
+      </button>
+      ;
+      <button type="button" onClick={() => handleClick('debug')}>
+        Debug
+      </button>
+      ;
+      <button type="button" onClick={() => handleClick('info')}>
+        Info
+      </button>
+      ;
+      <button type="button" onClick={() => handleClick('warn')}>
+        Warning
+      </button>
+      ;
+      <button type="button" onClick={() => handleClick('error')}>
+        Error
+      </button>
+      ;
+      <button type="button" onClick={() => handleClick('fatal')}>
+        Fatal
+      </button>
+      ;
+    </>
+  );
 }
 
 export default App;
--- libs/logging/src/types/logging.ts
+++ libs/logging/src/types/logging.ts
@@ -2,11 +2,11 @@ import type { SeverityLevel } from '@sentry/types/types/severity';
 import type { Hub } from '@sentry/types';
 
 export interface CallbackData {
-	error?: unknown;
-	level?: string;
-	message?: string;
-	meta?: unknown;
-	objects?: unknown | unknown[];
+  error?: unknown;
+  level?: string;
+  message?: string;
+  meta?: unknown;
+  objects?: unknown | unknown[];
 }
 
 export type LogCallback = (data: CallbackData) => void;
@@ -14,17 +14,17 @@ export type LogCallback = (data: CallbackData) => void;
 export type LogMetaInformation = Record<string, unknown>;
 
 export interface LogOptions {
-	callback?: LogCallback;
-	meta?: LogMetaInformation;
-	objects?: unknown | unknown[];
-	tags?: string[];
-	error?: unknown;
+  callback?: LogCallback;
+  meta?: LogMetaInformation;
+  objects?: unknown | unknown[];
+  tags?: string[];
+  error?: unknown;
 }
 
 export type GlobalLogOptions = Pick<LogOptions, 'tags' | 'meta' | 'callback'>;
 
 export interface LeveledLogMethod {
-	(message: string, options?: LogOptions): void;
+  (message: string, options?: LogOptions): void;
 }
 
 export type LogLevel = 'fatal' | 'error' | 'warn' | 'info' | 'debug' | 'trace';
@@ -32,63 +32,63 @@ export type LogLevel = 'fatal' | 'error' | 'warn' | 'info' | 'debug' | 'trace';
 export type Environment = 'never' | 'local' | 'develop' | 'staging' | 'production';
 
 export type EnvironmentLevelMap = {
-	[key in LogLevel]?: Environment;
+  [key in LogLevel]?: Environment;
 };
 
 export interface LoggerOptions {
-	environment?: Environment;
-	globalLogOptions?: GlobalLogOptions;
-	transports?: ITransport[];
+  environment?: Environment;
+  globalLogOptions?: GlobalLogOptions;
+  transports?: ITransport[];
 }
 
 export type ExecutionContext = 'node' | 'browser';
 
 export type ColorLevelMap = {
-	[key in LogLevel]: (message: string) => string;
+  [key in LogLevel]: (message: string) => string;
 };
 
 export type ExecutionContextColorMap = {
-	[key in ExecutionContext]: ColorLevelMap;
+  [key in ExecutionContext]: ColorLevelMap;
 };
 
 /**
  * Reduced interface for exchange ability
  */
 export interface ILogger {
-	fatal: LeveledLogMethod;
-	error: LeveledLogMethod;
-	warn: LeveledLogMethod;
-	info: LeveledLogMethod;
-	debug: LeveledLogMethod;
-	trace: LeveledLogMethod;
+  fatal: LeveledLogMethod;
+  error: LeveledLogMethod;
+  warn: LeveledLogMethod;
+  info: LeveledLogMethod;
+  debug: LeveledLogMethod;
+  trace: LeveledLogMethod;
 
-	withOptions(options: LoggerOptions): ILogger;
+  withOptions(options: LoggerOptions): ILogger;
 }
 
 export interface TransportOptions {
-	environmentLevelMap?: EnvironmentLevelMap;
-	transportLogOptions?: GlobalLogOptions;
+  environmentLevelMap?: EnvironmentLevelMap;
+  transportLogOptions?: GlobalLogOptions;
 }
 
 export interface SentryConfig {
-	dsn: string;
-	release?: string;
-	tracesSampleRate?: number;
+  dsn: string;
+  release?: string;
+  tracesSampleRate?: number;
 }
 
 export interface SentryTransportOptions extends TransportOptions {
-	sentryConfig: SentryConfig;
+  sentryConfig: SentryConfig;
 }
 
 export interface ITransport {
-	send: (level: LogLevel, message: string, options?: LogOptions) => void;
-	setup: (executionContext: ExecutionContext, environment: Environment, globalLogOptions: GlobalLogOptions) => void;
+  send: (level: LogLevel, message: string, options?: LogOptions) => void;
+  setup: (executionContext: ExecutionContext, environment: Environment, globalLogOptions: GlobalLogOptions) => void;
 }
 
 export type SentryLogLevelMap = {
-	[key in LogLevel]: SeverityLevel;
+  [key in LogLevel]: SeverityLevel;
 };
 
 export type SentrySdk = Hub & {
-	init: (sentryConfig: SentryConfig) => void;
+  init: (sentryConfig: SentryConfig) => void;
 };
