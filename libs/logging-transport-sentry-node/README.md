8878d1be5afb8840601af8e08752f451ebfaf13f
Gordon Schroeder <gordon.schroeder@alchemisten.ag>
2022-08-15 15:07:18 +0200

    Feat: Add sentry node transport

---
+++ libs/logging-transport-sentry-node/README.md
@@ -0,0 +1,11 @@
+# logging-transport-sentry-node-logging-transport-sentry-node
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build logging-transport-sentry-node-logging-transport-sentry-node` to build the library.
+
+## Running unit tests
+
+Run `nx test logging-transport-sentry-node-logging-transport-sentry-node` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/logging-transport-sentry-node/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'logging-transport-sentry-node',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/logging-transport-sentry-node',
+};
