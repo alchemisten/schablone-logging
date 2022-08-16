0d5caa1a9dae46f5786d84ee936b8ad9224c2098
Gordon Schroeder <gordon.schroeder@alchemisten.ag>
2022-08-16 11:23:32 +0200

    refactor: Split types into multiple files

--- libs/logging/src/types/index.ts
+++ libs/logging/src/types/index.ts
@@ -1,1 +1,4 @@
+export * from './base';
+export * from './transports';
+export * from './transport-sentry';
 export * from './logging';
--- libs/logging/src/types/logging.ts
+++ libs/logging/src/types/logging.ts
@@ -1,56 +1,15 @@
-import type { SeverityLevel } from '@sentry/types/types/severity';
-import type { BrowserOptions } from '@sentry/browser';
-import type { NodeOptions } from '@sentry/node';
-
-export interface CallbackData {
-  error?: unknown;
-  level: string;
-  message: string;
-  meta?: unknown;
-  objects?: unknown | unknown[];
-}
-
-export type LogCallback = (data: CallbackData) => void;
-
-export type LogMetaInformation = Record<string, unknown>;
-
-export interface LogOptions {
-  callback?: LogCallback;
-  meta?: LogMetaInformation;
-  objects?: unknown | unknown[];
-  tags?: string[];
-  error?: unknown;
-}
-
-export type GlobalLogOptions = Pick<LogOptions, 'tags' | 'meta' | 'callback'>;
+import { Environment, GlobalLogOptions, LogOptions } from './base';
+import { ITransport } from './transports';
 
 export interface LeveledLogMethod {
   (message: string, options?: LogOptions): void;
 }
-
-export type LogLevel = 'fatal' | 'error' | 'warn' | 'info' | 'debug' | 'trace';
-
-export type Environment = 'never' | 'local' | 'develop' | 'staging' | 'production';
-
-export type EnvironmentLevelMap = {
-  [key in LogLevel]?: Environment;
-};
 
 export interface LoggerOptions {
   environment?: Environment;
   globalLogOptions?: GlobalLogOptions;
   transports?: ITransport[];
 }
-
-export type ExecutionContext = 'node' | 'browser';
-
-export type ColorLevelMap = {
-  [key in LogLevel]: (message: string) => string;
-};
-
-export type ExecutionContextColorMap = {
-  [key in ExecutionContext]: ColorLevelMap;
-};
 
 /**
  * Reduced interface for exchange ability
@@ -65,23 +24,3 @@ export interface ILogger {
 
   withOptions(options: LoggerOptions): ILogger;
 }
-
-export interface TransportOptions {
-  environmentLevelMap?: EnvironmentLevelMap;
-  transportLogOptions?: GlobalLogOptions;
-}
-
-export type SentryConfig = BrowserOptions | NodeOptions;
-
-export interface SentryTransportOptions extends TransportOptions {
-  sentryConfig: SentryConfig;
-}
-
-export interface ITransport {
-  send: (level: LogLevel, message: string, options?: LogOptions) => void;
-  setup: (executionContext: ExecutionContext, environment: Environment, globalLogOptions: GlobalLogOptions) => void;
-}
-
-export type SentryLogLevelMap = {
-  [key in LogLevel]: SeverityLevel;
-};
