775e0c6beb8528585ba02b98982362c74c2041a1
Gordon Schroeder <gordon.schroeder@alchemisten.ag>
2023-02-07 14:41:48 +0100

    docs: Add documentation for ITransport clone function

--- libs/logging/src/types/transports.ts
+++ libs/logging/src/types/transports.ts
@@ -23,6 +23,11 @@ export interface TransportOptions {
  * configuration and sending messages of all log levels.
  */
 export interface ITransport {
+  /**
+   * Called when a logger is derived from via its withOptions() function to
+   * copy the transports if no new transports are initialized. Should return a
+   * cloned copy of the transport with identical settings but no references.
+   */
   clone: () => ITransport;
   /**
    * Called if the transport is active in a logger and a message is logged.
